715850c9ae386b33bead3feba3560e13179fa7e0
Tanwin Chowdary Gunturu <160269367+TanwinChowdary@users.noreply.github.com>
2025-04-21 11:50:43 -0500

    Update build_index.py

--- src/build_index.py
+++ src/build_index.py
@@ -1,12 +1,13 @@
 import os
 import sys
-sys.path.insert(0, '/IRS_Helpdesk_RAGchatbot/src')  # Or sys.path.append()
-
+# sys.path.insert(0, '/IRS_Helpdesk_RAGchatbot/src')  # Or sys.path.append()
+import embed
+import perprocess
 import faiss
 import numpy as np
 import pickle
-from src.embed import get_embedding
-from src.preprocess import chunk_text
+# from src.embed import get_embedding
+# from src.preprocess import chunk_text
 # sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'src')))
 def build_faiss_index(data_dir):
     # Read and process the IRS data (FAQs, PDFs, etc.)
@@ -16,11 +17,11 @@ def build_faiss_index(data_dir):
             with open(os.path.join(data_dir, file_name), 'r') as file:
                 # Chunk the text into smaller sections
                 text = file.read()
-                chunked_texts = chunk_text(text)
+                chunked_texts = perprocess.chunk_text(text)
                 texts.extend(chunked_texts)
     
     # Get embeddings for the chunked text data
-    embeddings = np.array([get_embedding(text) for text in texts], dtype=np.float32)
+    embeddings = np.array([embed.get_embedding(text) for text in texts], dtype=np.float32)
     
     # Build the FAISS index for fast retrieval
     index = faiss.IndexFlatL2(embeddings.shape[1])
